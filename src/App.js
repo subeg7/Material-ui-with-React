b96b9d65be0e37c4b89f09f65eabb7747065733b
Subeg7 <subeg7@gmail.com>
2019-10-07 14:40:37 +0545

    cell component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Cell from "./Components/Cell";
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+const CellLength = 50;
+const CellProps={
+  height:CellLength,
+  width:CellLength,
 }
 
-export default App;
+export default class App extends Component {
+  render() {
+    // Stage is a div container
+    // Layer is actual canvas element (so you may have several canvases in the stage)
+    // And then we have canvas shapes inside the Layer
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+            <Cell position = {{x:0,y:0}} size= {CellProps}/>
+            <Cell position = {{x: 60,y:0}} size= {CellProps}/>
+            <Cell position = {{x: 120,y:0}} size= {CellProps}/>
+            <Cell position = {{x: 180,y:0}} size= {CellProps}/>
+            <Cell position = {{x: 240,y:0}} size= {CellProps}/>
+        </Layer>
+      </Stage>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
---
+++ src/Components/Cell.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
+
+export default class Cell extends React.Component {
+    state = {
+      color: 'green'
+    };
+    // handleClick = () => {
+    //   this.setState({
+    //     color: Konva.Util.getRandomColor()
+    //   });
+    // };
+    render() {
+      return (
+        <Rect
+          x={this.props.position.x}
+          y={this.props.position.y}
+
+          width={this.props.size.width}
+          height={this.props.size.height}
+
+          fill={this.state.color}
+        //   shadowBlur={5}
+          onClick={this.handleClick}
+        />
+      );
+    }
+  }
